8530a6a8e9378e8817a8bf2bef8273d2cad2ad88
yanJuicy <hanjub3@gmail.com>
2023-08-04 14:53:00 +0900

    목록을 보여주닌 리스트뷰

---
+++ listview_example/lib/animal_item.dart
@@ -0,0 +1,13 @@
+class Animal {
+  String? imagePath;
+  String? animalName;
+  String? kind;
+  bool? flyExist = false;
+
+  Animal({
+    required this.animalName,
+    required this.kind,
+    required this.imagePath,
+    this.flyExist,
+  });
+}
